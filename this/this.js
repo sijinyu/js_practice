8e4d462c35faf07d77997eb5b952b6236d00a592
sijinyu <sijinyudev@gmail.com>
2022-09-09 12:34:56 +0900

    refactor: this.js 결과값 보기 쉽게 수정

--- this/this.js
+++ this/this.js
@@ -1,22 +1,18 @@
 const someone = {
-  name: "Someone",
+  name: "시진아",
   sayHello: function (otherName) {
     console.log(this.name + " says hello to " + otherName);
   },
 };
 
-someone.sayHello("thomson");
-// 결과 > 'Someone says hello to thomson'
-const alex = {
-  name: "Alex",
+someone.sayHello("행배야");
+// 결과 > '시진아 says hello to 행배야'
+const sijin = {
+  name: "유시진아",
 };
 
-someone.sayHello.call(alex, "thomson");
-// 'Alex says hello to thomson'
+someone.sayHello.call(sijin, "thomson");
+// '유시진아 says hello to 행배야'
 
-someone.sayHello.apply(alex, ["thomson"]);
-// 'Alex says hello to thomson'
-
-/*
-
-*/
+someone.sayHello.apply(sijin, ["thomson"]);
+// '유시진아 says hello to 행배야'
